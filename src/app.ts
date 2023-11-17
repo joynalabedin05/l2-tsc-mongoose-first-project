c211f4001b3fbe4c220bb107c8b5f29e9d6bedc6
joynalabedin05 <joynal05101993@gmail.com>
2023-11-17 11:59:11 +0600

    basic projects setup

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Application, Request, Response } from 'express';
+const app: Application = express();
+import cors from 'cors';
+
+// parser
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  const a = 10;
+  res.send(a);
+});
+
+console.log(process.cwd());
+
+export default app;
